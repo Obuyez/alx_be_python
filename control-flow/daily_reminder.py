2dd46b845de04a840e11621f1cf01d331cf8e14f
Obuyez <156541395+Obuyez@users.noreply.github.com>
2025-06-02 13:37:55 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,12 +3,24 @@ priority = input("Priority (high/medium/low): ").lower()
 time = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
-    case "high"| "medium" | "low":
+    case "high":
         if time == "yes":
-          print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
-             print(f"Reminder: '{task}' is a {priority} priority task. Please make sure to complete it.")
+            print(f"Reminder: '{task}' is a high priority task. Please make sure to complete it.")
     
+    case "medium":
+        if time == "yes":
+            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
+        else:
+            print(f"Reminder: '{task}' is a medium priority task. Please make sure to complete it.")
+
+    case "low":
+        if time == "yes":
+            print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
+        else:
+            print(f"Reminder: '{task}' is a low priority task. Please make sure to complete it.")
+
     case _:
-        print("Invalid input for priority! Please enter high, medium, or low. ")
+        print("Invalid input for priority! Please enter high, medium, or low.")
 
